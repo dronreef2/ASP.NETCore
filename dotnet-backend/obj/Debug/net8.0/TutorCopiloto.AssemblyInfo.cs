cfff6af49a76216a31d560ffb3780be4b7662e5a
Guilherme Naschold <202001579176@alunos.estacio.br>
2025-08-22 04:24:08 +0000

    Remove remaining duplicate test artifacts

--- dotnet-backend/obj/Debug/net8.0/TutorCopiloto.AssemblyInfo.cs
+++ dotnet-backend/obj/Debug/net8.0/TutorCopiloto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TutorCopiloto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e4ad101ccb67651f476030055d509547efc700c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b393929506d23be42c2d2e5b622b998c9fa30d30")]
 [assembly: System.Reflection.AssemblyProductAttribute("TutorCopiloto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TutorCopiloto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
